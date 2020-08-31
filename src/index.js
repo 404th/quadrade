2473185b4ab09c164d75278678d22f3cb0be18fd
myevenway <umarov.doniyor.2002@gmail.com>
2020-08-31 20:08:41 +0500

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
-      App 
+      Index
     </div>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-serviceWorker.unregister();
+)
