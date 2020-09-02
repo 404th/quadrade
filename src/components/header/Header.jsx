264a6af2e063353fbb4668757bdd955226003801
myevenway <umarov.doniyor.2002@gmail.com>
2020-09-02 12:23:44 +0500

    Some changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,8 +18,8 @@ const Header = () => {
   }
 
   return (
-    <header className='container-fluid p-3' >
-      <nav className="navbar navbar-expand-md p-0 m-0 container-lg row container-fluid">
+    <header className='container-fluid p-lg-3 p-md-3 p-sm-0' >
+      <nav className="navbar navbar-expand-md p-lg-0 p-md-3 p-sm-3 m-0 container-lg row container-fluid">
         <NavLink className="navbar-brand navbar_brand_logo col-2" to="/">
           <span>Alivio</span>
         </NavLink>
@@ -29,10 +29,10 @@ const Header = () => {
         </button>
 
         <div className="collapse navbar-collapse col-10 p-0 row navbar_items_row" id="collapsibleNavbar">
-          <ul className="navbar-nav col-9">
+          <ul className="navbar-nav col-lg-9 col-md-9 col-sm-10">
             { setLi() }
           </ul>
-          <ul className='navbar-nav navbar_forma p-0 col-3'>
+          <ul className='navbar-nav navbar_forma p-0 col-lg-3 col-md-3 col-sm-2'>
             <li className='nav-item navbar_forma_item' >
               <NavLink className='nav-link signIn' to='/signIn'>Sign in</NavLink>
             </li>
