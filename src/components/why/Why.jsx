3ce2792ec88000809c3bc1a79fc5ecc3f2060db5
myevenway <umarov.doniyor.2002@gmail.com>
2020-09-08 00:47:59 +0500

    Some changes on Project

--- src/components/why/Why.jsx
+++ src/components/why/Why.jsx
@@ -8,12 +8,12 @@ const Why = () => {
     <div className='container-fluid why_page'>
       <div className="container why_page_container">
         <div className="row why_page_container_row">
-          <div className="col-lg-4 p-0 why_page_container_row_first">
-            <h2>{ WhyContents[0].title }</h2>
-            <p>{ WhyContents[1].title }</p>
-            <div className={'p-lg-3 p-md-3 p-sm-2'}>{ WhyContents[2].title }</div>
+          <div className="col-lg-4 col-md-12 col-sm-12 p-0 why_page_container_row_first">
+            <h2 className={'my-lg-4 my-md-2 my-sm-1'} >{ WhyContents[0].title }</h2>
+            <p className={'my-lg-4 my-md-2 my-sm-1'} >{ WhyContents[1].title }</p>
+            <div className={'p-lg-3 p-md-3 p-sm-2 my-lg-4 my-md-2 my-sm-1'}>{ WhyContents[2].title }</div>
           </div>
-          <div className="col-lg-8 why_page_container_row_second">
+          <div className="col-lg-8 col-md-12 col-sm-12 why_page_container_row_second">
             <div className="why_page_container_row_second_cover">
               <iframe src="https://www.youtube.com/embed/rmqvHAaJD54"
                       title="YouTube Hitler chumo video"
